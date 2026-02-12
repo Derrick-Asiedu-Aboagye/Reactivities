a13f7f2e7dc1c87b80e7af44b98cb26bb75302d5
Derrick <derrickasiedu82@gmail.com>
2026-02-12 13:21:21 +0200

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,10 +9,19 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    // Ensure BOTH http and https versions of your React app are here
+    .WithOrigins("http://localhost:3000")
+    // , "https://localhost:3000")
+);
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
