b6c9fbe61bdfba6c30b40ee64ca3c63babef42a6
Derrick <derrickasiedu82@gmail.com>
2026-02-23 09:01:54 +0200

    End of Section 15

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,12 +1,13 @@
 import { Box, Container, CssBaseline } from "@mui/material";
 import NavBar from "./NavBar";
-import { Outlet, useLocation } from "react-router";
+import { Outlet, ScrollRestoration, useLocation } from "react-router";
 import Homepage from "../../features/home/Homepage";
 
 export default function App() {
   const location = useLocation();
   return (
-    <Box sx={{bgcolor: '#eeeeee', minHeight: '100vh'}}>
+    <Box sx={{ bgcolor: '#eeeeee', minHeight: '100vh' }}>
+      <ScrollRestoration />
       <CssBaseline />
       {location.pathname === '/' ? <Homepage /> :
       (
